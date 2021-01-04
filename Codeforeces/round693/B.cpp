7d785573a000b23a001bfcdcbb4ef7111ee72967
Shubham <smartshubhampathak@gmail.com>
2021-01-04 23:17:01 +0530

    codeforces round 693

--- Codeforeces/round693/B.cpp
+++ Codeforeces/round693/B.cpp
@@ -23,7 +23,16 @@ void divideAmongTwo(int x[], int n)
     }
     else
     {
-        if(numOf1s==2*numOf2s){
+        //if numOf1s is odd then it cannot be divided into 2 sets
+        //if numOf2s is odd then it can be divided into 2 sets only if numOf1s > 0
+        //ex 2 2 2 1 1 1 1  then (2,,1,2) (2,1,1,1)
+        //ex 2 2 2 1 odd no. of 1 NO
+        //ex 2 2 2 1 1 then (2,2) (2,1,1)
+        //basically when numOf1s is even and numOf2s is odd then 2 2 2 1 1 1 1 => 2 | 2 2 1 1 1 1 (makes 2 also even no it can be divided into 2 sets)
+        //for the remaining 2 if you add into 1 set and take 1 out of it to add into anther 
+        // 2 (2,1,1) (2,1,1) => (2,2) (2,1,1,1)
+        // (x) (x) => (x -1 +2) (x +1) => (x+1) (x+1) still equal
+        if(numOf1s%2==0 && numOf1s>0){
             cout << "YES" << endl;
         }else{
             cout << "NO" << endl;
